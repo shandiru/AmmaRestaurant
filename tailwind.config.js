dea3840f9fccd84fc50ce62d56afe58ded213c7a
shandiru <msddiru@gmail.com>
2025-12-21 20:09:29 +0530

    added

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        cormorant: ["Cormorant Upright", "serif"],
+        opensans: ["Open Sans", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
