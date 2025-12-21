bc683d5d6c517e5534b9bae9f22292b6eac82f33
shandiru <msddiru@gmail.com>
2025-12-21 20:49:01 +0530

    added

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -7,14 +7,25 @@ import './Header.css';
 const Header = () => (
   <div className="app__header app__wrapper section__padding" id="home">
     <div className="app__wrapper_info">
-      <SubHeading title="Chase the new flavour" />
-      <h1 className="app__header-h1">The Key To Fine Dining</h1>
-      <p className="p__opensans" style={{ margin: '2rem 0' }}>Sit tellus lobortis sed senectus vivamus molestie. Condimentum volutpat morbi facilisis quam scelerisque sapien. Et, penatibus aliquam amet tellus </p>
-      <button type="button" className="custom__button">Explore Menu</button>
+      <SubHeading title="Authentic South Indian Taste" />
+
+      <h1 className="app__header-h1">
+        Amma Kitchen Coventry
+      </h1>
+
+      <p className="p__opensans" style={{ margin: '2rem 0' }}>
+        Experience the warmth of home-style cooking at Amma Kitchen Coventry.
+        We serve freshly prepared, authentic South Indian dishes made with
+        traditional recipes and love.
+      </p>
+
+      <button type="button" className="custom__button">
+        View Our Menu
+      </button>
     </div>
 
     <div className="app__wrapper_img">
-      <img src={images.welcome} alt="header_img" />
+      <img src={images.welcome} alt="Amma Kitchen Coventry" />
     </div>
   </div>
 );
